458ac4452a7a53067e1b91df76f0b67aebde1890
Mayurimk98 <mayurimkatwe@gmail.com>
2023-06-15 09:05:45 +0530

    15 june

---
+++ Script9.js
@@ -0,0 +1,84 @@
+// let m1 = [12, 34, 56, 89, 78, 50]
+// //         0   1   2   3   4   5
+
+
+// m1.slice(2)
+
+
+//23.sort()
+
+// let city=["nashik","pune","kolhapur",'satana',"nagpur"]
+// let a=city.sort()
+// console.log(a)
+
+
+// let score = [12, 56, 300, 799, 233, 900, 8]
+// // [8,12,56,233,300,799,900]
+
+// // let y2 = score.sort()
+// // console.log(y2)
+
+// let y3 = score.sort(function (a, b) {
+//     return a - b
+// })
+// console.log(y3)
+
+
+//a-b ==> positive => b a
+
+//a-b ==> negetive => a b
+
+//a-b =>   zero => no change
+
+
+//=======================================================================
+
+
+//String
+
+let firstName="mayuri"
+console.log(typeof firstName)
+
+let fn1='vaibhav'
+console.log(typeof fn1)
+
+let fn2=`jaya`
+console.log(typeof fn2)
+
+let fName="mayuri katwe"
+
+// m    a   y   u   r   i       k   a   t   w   e
+// 0    1   2   3   4   5   6   7   8    9  10 11
+
+console.log(fName[4])
+
+console.log(fName[6])
+
+//object ==> property and methods
+
+//string => object => property and methods
+
+//length  => property
+
+console.log(fName.length) //12
+
+
+console.log(fName.length-1) //11
+
+console.log(fName[fName.length-1]) //e
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
