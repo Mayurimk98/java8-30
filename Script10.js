e1c778b51856edcbeda1085c9f8f714f421e741f
Mayurimk98 <mayurimkatwe@gmail.com>
2023-06-17 21:05:20 +0530

    17 june

---
+++ Script10.js
@@ -0,0 +1,123 @@
+
+
+// let a=20
+// console.log(typeof a) //number
+
+// let b="true"
+// console.log(typeof b)//string
+
+// let c='200'
+// console.log(typeof c) //string
+
+// let d=`mayuri`
+
+// console.log(typeof d)
+// //tab
+
+// let d1=""
+// console.log(d1)
+
+// let f1=[]
+
+
+//javascript ==> object ==> property and methods
+
+//String => object 
+
+// let str="Shree Ganesh"
+// //indexvice
+
+// //S   h   e   e   e       G   a   n   e   s   h
+// //0   1   2   3   4   5   6   7   8   9   10  11  => index
+// //1   2   3   4   5   6   7   8   9   10   11  12 => length
+
+// console.log(str[0])  //S
+
+// console.log(str[5])  //
+
+// console.log(str[3]) //e
+
+// console.log(str.length) //12
+
+// //firstchar 
+// console.log(str[0])//first char of string==> S
+
+// //lastChar
+// console.log(str[11]) //h
+
+// console.log(str[str.length-1])  //h
+
+
+// //length
+
+// let a1=[1,2,3,4,5] //number of element
+
+// let b1="mayuri" //number of char
+
+
+//methods
+
+
+// let str="Shree Ganesh"
+// //indexvice
+
+// //S   h   e   e   e       G   a   n   e   s   h
+// //0   1   2   3   4   5   6   7   8   9   10  11  => index
+// //1   2   3   4   5   6   7   8   9   10   11  12 => length
+
+//1. toUpperCase()
+//action  => convert all char of string into capital case
+//return  => new string
+
+// let y1=str.toUpperCase()
+// console.log(y1)
+
+//insta ==> acc create
+//log in sign in
+
+//database
+
+//2. toLowerCase()
+//action => convert all char of string into lowercase
+//return => new string
+
+// let y2=str.toLowerCase()
+// console.log(y2)
+
+
+let str="Shree Ganesh"
+//indexvice
+
+//S   h   r   e   e       G   a   n   e   s   h
+//0   1   2   3   4   5   6   7   8   9   10  11  => index
+//1   2   3   4   5   6   7   8   9   10   11  12 => length
+
+//3. includes()
+//action => check whether given char is present in string or not
+//return  => boolean
+
+// let u1=str.includes('s')
+// console.log(u1)//true
+
+// console.log(str.includes('e'))//true
+
+// console.log(str.includes(" "))//true
+
+
+//4. indexOf()
+//action => retun the index of given char
+//return  => number
+
+let p1=str.indexOf('e')
+
+console.log(p1)//3
+
+console.log(str.indexOf('g'))//-1
+
+
+
+
+
+
+
+
